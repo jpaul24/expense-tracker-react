b05ef0f996cc85f5407de90cffca34ad04842b3c
Joe Paul <joe.paul0712@gmail.com>
2020-03-12 15:56:14 +0000

    modify document

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,20 +1,20 @@
-import React, { useContext } from 'react';
-import { Transaction } from './Transaction';
+import React, { useContext } from "react";
+import { Transaction } from "./Transaction";
 
-import { GlobalContext } from '../context/GlobalState';
+import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
-    const { transactions } = useContext(GlobalContext);
+  const { transactions } = useContext(GlobalContext);
 
-    // console.log(context)
-    return (
-        <>
-            <h3>History</h3>
-            <ul className="list">
-                {transactions.map(transaction => (
-                    <Transaction key={transaction.id} transaction={transaction}/>
-                ))}
-            </ul>
-        </>
-    )
-}
+  // console.log(context)
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
+    </>
+  );
+};
